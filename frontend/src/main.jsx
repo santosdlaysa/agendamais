618ab86c1eb0ed512dba64ee50d1f6a0b6b7e387
ldsantos <laysa.santos@roraimaenergia.com.br>
2026-01-21 14:06:25 -0400

    Implementa otimizações SEO: meta tags, Open Graph, sitemap e BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
@@ -13,7 +13,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AuthProvider>
         <SubscriptionProvider>
           <OnboardingProvider>
@@ -37,6 +37,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           />
         </SubscriptionProvider>
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
